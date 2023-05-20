a0a72d825bd59964edc20c7a35e5d8649ff8cd61
Gabriele Iannace <57633646+gabrieleiannace@users.noreply.github.com>
2023-05-20 06:19:59 +0200

    Scrollable

--- app/src/main/java/com/example/courtreservationapplicationjetpack/views/courts/AllSports.kt
+++ app/src/main/java/com/example/courtreservationapplicationjetpack/views/courts/AllSports.kt
@@ -34,9 +34,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.outlined.DateRange
@@ -48,11 +51,13 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -73,6 +78,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
@@ -86,6 +92,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.chargemap.compose.numberpicker.NumberPicker
 import com.example.courtreservationapplicationjetpack.R
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -431,52 +438,74 @@ fun CoilImage(modifier: Modifier = Modifier, sport: String) {
 }
 
 //--------------------------------------------------------------------------------
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Ciao(){
-    Box(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .background(Color.Red)
-                .align(Alignment.TopCenter)
-        )
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data("https://www.parrocchiecurtatone.it/wp-content/uploads/2020/07/WhatsApp-Image-2020-07-23-at-17.53.36-1984x1200.jpeg")
-                .crossfade(true)
-                .build(),
-            placeholder = painterResource(R.drawable.placeholder),
-            contentDescription = "Court Image",
-            contentScale = ContentScale.Crop,
-        )
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    brush = Brush.verticalGradient(
-                        0f to Color.Transparent,
-                        0.0001f to Color.Transparent,
-                        1f to Color.Black
-                    )
-                )
-                .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
-        )
+fun Ciao() {
+    val lazyListState = rememberLazyListState()
+    val firstItemTranslationY by remember {
+        derivedStateOf {
+            when {
+                lazyListState.layoutInfo.visibleItemsInfo.isNotEmpty() &&
+                        lazyListState.firstVisibleItemIndex == 0 ->
+                    lazyListState.firstVisibleItemScrollOffset * .7f
+                else -> 0f
+            }
+        }
+    }
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .offset(y = (185).dp)
-                .background(MaterialTheme.colorScheme.surface, MaterialTheme.shapes.large)
-        ) {
-            Column {
-                Box {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        state = lazyListState
+    ) {
+        item {
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(180.dp)
+                        .background(Color.Red)
+                        .align(Alignment.TopCenter)
+                )
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://www.parrocchiecurtatone.it/wp-content/uploads/2020/07/WhatsApp-Image-2020-07-23-at-17.53.36-1984x1200.jpeg")
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(R.drawable.placeholder),
+                    contentDescription = "Court Image",
+                    contentScale = ContentScale.Crop,
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                0f to Color.Transparent,
+                                0.0001f to Color.Transparent,
+                                1f to Color.Black
+                            )
+                        )
+                        .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
+                )
+            }
+        }
+
+        item {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = MaterialTheme.shapes.large.copy(
+                            topStart = CornerSize(10.dp),
+                            topEnd = CornerSize(10.dp)
+                        )
+                    )
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.padding(16.dp)
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         Column(modifier = Modifier.weight(3f)) {
                             Text(
@@ -506,21 +535,147 @@ fun Ciao(){
                             )
                         }
                     }
+
+                    Divider(
+                        color = Color.LightGray,
+                        thickness = 1.dp,
+                        modifier = Modifier
+                            .padding(top = 8.dp)
+                            .height(1.dp)
+                            .shadow(elevation = 4.dp, shape = MaterialTheme.shapes.medium)
+                    )
+
+                    CalendarScreen()
                 }
+            }
+        }
 
-                Divider(
-                    color = Color.LightGray,
-                    thickness = 1.dp,
-                    modifier = Modifier
-                        .padding(start = 16.dp, end = 16.dp, top = 8.dp)
-                        .height(1.dp)
-                        .shadow(elevation = 4.dp, shape = MaterialTheme.shapes.medium)
-                )
-                CalendarScreen()
+        item {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp)
+            ) {
+                Text(text = "Save")
+            }
+        }
 
+        item {
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp)
+            ) {
+                Text(text = "Delete")
             }
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    Box(modifier = Modifier.fillMaxSize()) {
+//        Box(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(200.dp)
+//                .background(Color.Red)
+//                .align(Alignment.TopCenter)
+//        )
+//        AsyncImage(
+//            model = ImageRequest.Builder(LocalContext.current)
+//                .data("https://www.parrocchiecurtatone.it/wp-content/uploads/2020/07/WhatsApp-Image-2020-07-23-at-17.53.36-1984x1200.jpeg")
+//                .crossfade(true)
+//                .build(),
+//            placeholder = painterResource(R.drawable.placeholder),
+//            contentDescription = "Court Image",
+//            contentScale = ContentScale.Crop,
+//        )
+//        Box(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(
+//                    brush = Brush.verticalGradient(
+//                        0f to Color.Transparent,
+//                        0.0001f to Color.Transparent,
+//                        1f to Color.Black
+//                    )
+//                )
+//                .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
+//        )
+//
+//        //QUESTA
+//        Box(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .fillMaxHeight()
+//                .offset(y = (185).dp)
+//                .background(MaterialTheme.colorScheme.surface, MaterialTheme.shapes.large)
+//        ) {
+//            Column {
+//                Box {
+//                    Row(
+//                        verticalAlignment = Alignment.CenterVertically,
+//                        horizontalArrangement = Arrangement.SpaceBetween,
+//                        modifier = Modifier.padding(16.dp)
+//                    ) {
+//                        Column(modifier = Modifier.weight(3f)) {
+//                            Text(
+//                                text = "Centro Sportivo Robilant",
+//                                style = MaterialTheme.typography.headlineSmall,
+//                                fontWeight = FontWeight.Bold
+//                            )
+//                            Text(
+//                                text = "Piazza Generale, Torino",
+//                                style = MaterialTheme.typography.labelSmall,
+//                                color = Color.Gray,
+//                                textAlign = TextAlign.Start
+//                            )
+//                        }
+//                        Box(
+//                            modifier = Modifier
+//                                .weight(1f)
+//                                .align(Alignment.CenterVertically)
+//                        ) {
+//                            Image(
+//                                painter = painterResource(R.drawable.ic_calcio5),
+//                                contentDescription = "Sport icon",
+//                                colorFilter = ColorFilter.tint(Color.Black),
+//                                modifier = Modifier
+//                                    .size(29.dp)
+//                                    .align(Alignment.Center)
+//                            )
+//                        }
+//                    }
+//                }
+//
+//                Divider(
+//                    color = Color.LightGray,
+//                    thickness = 1.dp,
+//                    modifier = Modifier
+//                        .padding(start = 16.dp, end = 16.dp, top = 8.dp)
+//                        .height(1.dp)
+//                        .shadow(elevation = 4.dp, shape = MaterialTheme.shapes.medium)
+//                )
+//                CalendarScreen()
+//
+//            }
+//        }
+//    }
 }
 @Composable
 fun CalendarScreen() {
@@ -532,12 +687,13 @@ fun CalendarScreen() {
         Row(
             modifier = Modifier
                 .horizontalScroll(scrollState)
-                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .padding(horizontal = 0.dp, vertical = 8.dp)
         ) {
             val daysToShow = 100
             repeat(daysToShow) { index ->
                 val currentDate = startDate.plusDays(index.toLong())
-                val dayOfWeek = currentDate.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                val dayOfWeek =
+                    currentDate.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
                 val dayOfMonth = currentDate.dayOfMonth
                 val month = currentDate.month.getDisplayName(TextStyle.SHORT, Locale.getDefault())
 
@@ -551,18 +707,100 @@ fun CalendarScreen() {
             }
         }
         Row(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            modifier = Modifier.padding(horizontal = 0.dp, vertical = 8.dp)
         ) {
             Text(
-                text = "Timeslot disponibili per il giorno ${selectedDate.value.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))}",
+                text = "Timeslot disponibili per il giorno ${
+                    selectedDate.value.format(
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                    )
+                }",
                 style = MaterialTheme.typography.labelMedium,
                 color = Color.Gray
             )
         }
 
-        TextGrid(listOf("10:00","11:00","12:00","13:00","14:00","15:00","16:00"))
+        TextGrid(listOf("10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"))
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .padding(16.dp)
+        ) {
+            var pickerValue by remember { mutableStateOf(1) }
+
+            Text(
+                text = "Numero di persone coinvolte: $pickerValue",
+                style = MaterialTheme.typography.labelMedium,
+                color = Color.Gray,
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            )
+
+            NumberPicker(
+                modifier = Modifier
+                    .width(120.dp)
+                    .align(Alignment.CenterVertically),
+                value = pickerValue,
+                range = 0..10,
+                onValueChange = {
+                    pickerValue = it
+                },
+                dividersColor = Color.Black.copy(alpha = 0.7f),
+            )
+        }
+
+
     }
 }
+
+@Composable
+fun PeoplePicker(
+    selectedValue: Int,
+    onValueSelected: (Int) -> Unit
+) {
+    val numbers = (1..5).toList()
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Select number of people:",
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            numbers.forEach { number ->
+                val isSelected = number == selectedValue
+                val color = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface
+                val background = if (isSelected) MaterialTheme.colorScheme.inversePrimary else Color.Transparent
+
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .size(48.dp)
+                        .clickable { onValueSelected(number) }
+                        .background(background, CircleShape)
+                        .clip(CircleShape)
+                ) {
+                    Text(
+                        text = number.toString(),
+                        style = MaterialTheme.typography.labelMedium.copy(color = color),
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
 fun TextGrid(textList: List<String>) {
@@ -571,7 +809,9 @@ fun TextGrid(textList: List<String>) {
 
     Column {
         repeat(rows) { rowIndex ->
-            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 0.dp)) {
                 for (columnIndex in 0 until 5) {
                     val index = rowIndex * 5 + columnIndex
                     if (index < textList.size) {
@@ -695,7 +935,7 @@ private fun animateCircleColor(isSelected: Boolean): Color {
 @Preview(showBackground = true)
 @Composable
 fun CourtPreview(){
-    TextGrid(listOf("10:00","11:00","12:00","13:00","14:00","15:00","16:00"))
+    Ciao()
 }
 
 
